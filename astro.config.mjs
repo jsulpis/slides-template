d89899ee2b73fc7ec5195b4579d8c8d200baafa7
Julien Sulpis <julien.sulpis@gmail.com>
2022-10-31 20:12:50 +0100

    add a demo of a dynamic component with React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   base: "/slides-template",
+  integrations: [react(), tailwind()],
 });
