1126eb7eb5df82e22ad03699f31d890daf9f8850
Julien Sulpis <julien.sulpis@gmail.com>
2024-04-18 11:25:30 +0200

    add a demo of a dynamic component with React

---
+++ src/components/react/List.tsx
@@ -0,0 +1,23 @@
+import { Reorder } from "framer-motion";
+import { useState } from "react";
+import { ListItem } from "./ListItem";
+
+const initialItems = ["🍅 Tomato", "🥒 Cucumber", "🧀 Cheese", "🥬 Lettuce"];
+
+// Source : https://codesandbox.io/s/framer-motion-5-drag-to-reorder-lists-uonye
+export function List() {
+	const [items, setItems] = useState(initialItems);
+
+	return (
+		<Reorder.Group
+			axis="y"
+			onReorder={setItems}
+			values={items}
+			className="relative !list-none"
+		>
+			{items.map((item) => (
+				<ListItem key={item} item={item} />
+			))}
+		</Reorder.Group>
+	);
+}
